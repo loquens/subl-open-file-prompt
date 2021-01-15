91fb7027cd83e0cbfc01ebf6b631567fa145d8b8
loquens <loquens@yandex.ru>
2021-01-15 12:44:25 +0300

    Small fix: return focus to input panel after quick panel close

--- open_file_prompt.py
+++ open_file_prompt.py
@@ -146,6 +146,7 @@ class QuickPanelSuggestionsList:
     def quick_panel_on_close(self, selectedIndex):
         if selectedIndex == -1:
             self.setCurrentIndex(-1)
+            self.owner.window.focus_view(self.owner._ip)
         else:
             self.owner.on_file_selected(self.getFileNameByIndex(selectedIndex))
 
