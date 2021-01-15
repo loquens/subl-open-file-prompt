bd420f7724db297243e91be35d942665a3da114e
loquens <loquens@yandex.ru>
2021-01-15 11:49:57 +0300

    New suggestions mode - quick panel, and ability to control sort mode through parameters

--- README.md
+++ README.md
@@ -16,3 +16,16 @@ Suggested keybindings:
 
     { "keys": ["ctrl+o"], "command": "open_file_prompt" },
     { "keys": ["ctrl+shift+s"], "command": "save_file_prompt" },
+
+It is possible to add one or more arguments for both commands. For example:
+
+    { "keys": ["ctrl+o"], "command": "open_file_prompt", "args": { "suggestions_list_type": 0, "directories_first" : true } }
+
+Values for _suggestions\_list\_type_:
+ * 0 - scratch buffer (default)
+ * 1 - status line
+ * 2 - quick panel
+
+Values for _directories\_first_:
+ * true - show directories before files in suggestions list (default)
+ * false - show files and directories in alphabetical order
--- open_file_prompt.py
+++ open_file_prompt.py
@@ -14,9 +14,28 @@ def fileNameStartsWith(fileName, prefix):
     return fileName.startswith(prefix)
 
 
+def getDirectoryPresentation(files, currentDir, showDirsBeforeFiles=True):
+    files.sort()
+    dirs = []
+    filesOnly = []
+    for file in files:
+        if os.path.isdir(currentDir + os.sep + file):
+            # appending trailing os.sep to directories
+            directoryPresentation = file + os.sep
+            if showDirsBeforeFiles:
+                # Showing dirs before files
+                dirs.append(directoryPresentation)
+            else:
+                filesOnly.append(directoryPresentation)
+        else:
+            filesOnly.append(file)
+    files = dirs + filesOnly
+    return files
+
+
 class ScratchSuggestionsList:
-    def __init__(self, owner):
-        self.showDirsBeforeFiles = True
+    def __init__(self, owner, dirs_first):
+        self.showDirsBeforeFiles = dirs_first
         self.scratch_buffer = None
         self.owner = owner
 
@@ -32,6 +51,11 @@ class ScratchSuggestionsList:
                 window.run_command('close')
 
     def set_content(self, files, currentDir):
+        # sorting entries
+        files = getDirectoryPresentation(files,
+                                         currentDir,
+                                         self.showDirsBeforeFiles)
+
         if not self.scratch_buffer:
             # create scratch file list if it doesn't already exist
             self.scratch_buffer = self.owner.window.new_file()
@@ -42,29 +66,11 @@ class ScratchSuggestionsList:
 
         num_files = len(files)
 
-        # sorting entries
-        files.sort()
-
         vp_extent = self.scratch_buffer.viewport_extent()
         line_height = self.scratch_buffer.line_height()
         line_width = self.scratch_buffer.em_width()
         view_height_chars = int(math.floor(vp_extent[1] / line_height))
         view_width_chars = int(vp_extent[0] / line_width)
-
-        dirs = []
-        filesOnly = []
-        for file in files:
-            if os.path.isdir(currentDir + os.sep + file):
-                # appending trailing os.sep to directories
-                directoryPresentation = file + os.sep
-                if self.showDirsBeforeFiles:
-                    # Showing dirs before files
-                    dirs.append(directoryPresentation)
-                else:
-                    filesOnly.append(directoryPresentation)
-            else:
-                filesOnly.append(file)
-        files = dirs + filesOnly
 
         # getting maximum file name length in list
         maxFileNameLen = len(max(files, key=len))
@@ -104,6 +110,10 @@ class ScratchSuggestionsList:
 
 
 class StatusSuggestionsList:
+    def __init__(self, owner, dirs_first):
+        self.showDirsBeforeFiles = dirs_first
+        pass
+
     def clear(self):
         pass
 
@@ -111,18 +121,75 @@ class StatusSuggestionsList:
         pass
 
     def set_content(self, filesInDir, currentDir):
+        filesInDir = getDirectoryPresentation(
+            filesInDir,
+            currentDir,
+            self.showDirsBeforeFiles)
         statusText = ''.join((f + ', ') for f in filesInDir)
         statusText = statusText[:-2]
         statusText = '{ ' + statusText + ' }'
         sublime.status_message(statusText)
 
 
-class FilePromptCommand(sublime_plugin.WindowCommand):
-    def show_prompt(self, save_file, use_scratch_buffer):
-        if use_scratch_buffer:
-            self.suggestions_list = ScratchSuggestionsList(self)
+class QuickPanelSuggestionsList:
+    def __init__(self, owner, dirs_first):
+        self.showDirsBeforeFiles = dirs_first
+        self.quick_panel = None
+        self.owner = owner
+
+    def clear(self):
+        pass
+
+    def close(self):
+        pass
+
+    def quick_panel_on_close(self, selectedIndex):
+        if selectedIndex == -1:
+            self.setCurrentIndex(-1)
+        # Sublime Text 4 (4094) leaves scratch_buffer focused after update
+        # Sublime Text 3 - not
+        self.owner.window.focus_view(self.owner._ip)
+
+    def setCurrentIndex(self, currentIndex):
+        if currentIndex == -1:
+            text = self.currentDir + os.sep
         else:
-            self.suggestions_list = StatusSuggestionsList()
+            text = self.currentDir + os.sep + self.files[currentIndex]
+        self.owner._ip.run_command('update_input_panel', {'text': text})
+
+    def quick_panel_on_highlited(self, selectedIndex):
+        self.setCurrentIndex(selectedIndex)
+
+    def set_content(self, files, currentDir):
+        self.currentDir = currentDir
+        files = getDirectoryPresentation(files,
+                                         currentDir,
+                                         self.showDirsBeforeFiles)
+        self.files = files
+        self.owner.window.show_quick_panel(
+            [file for file in files],
+            self.quick_panel_on_close,
+            0,
+            0,
+            self.quick_panel_on_highlited)
+        self.setCurrentIndex(0)
+
+
+class FilePromptCommand(sublime_plugin.WindowCommand):
+    SUGGS_SCRATCH = 0
+    SUGGS_STATUS = 1
+    SUGGS_QUICK_PANEL = 2
+
+    def show_prompt(self,
+                    save_file,
+                    suggestions_list_type,
+                    dirs_first):
+        if suggestions_list_type == FilePromptCommand.SUGGS_SCRATCH:
+            self.suggs_list = ScratchSuggestionsList(self, dirs_first)
+        elif suggestions_list_type == FilePromptCommand.SUGGS_STATUS:
+            self.suggs_list = StatusSuggestionsList(self, dirs_first)
+        elif suggestions_list_type == FilePromptCommand.SUGGS_QUICK_PANEL:
+            self.suggs_list = QuickPanelSuggestionsList(self, dirs_first)
 
         currentDir = self.getStartDir()
         if save_file:
@@ -169,7 +236,7 @@ class FilePromptCommand(sublime_plugin.WindowCommand):
             if filesInDir:
                 matchCount = len(filesInDir)
                 if matchCount > 1:
-                    self.suggestions_list.set_content(filesInDir, currentDir)
+                    self.suggs_list.set_content(filesInDir, currentDir)
 
                     prefix = os.path.commonprefix(filesInDir)
                     lowerFiles = [x.lower() for x in filesInDir]
@@ -187,15 +254,16 @@ class FilePromptCommand(sublime_plugin.WindowCommand):
                 newPath = currentFilePath
                 sublime.status_message(
                      'No files match "%s"' % currentFile)
-                self.suggestions_list.clear()
+                self.suggs_list.clear()
 
             if os.path.isdir(newPath) and matchCount == 1:
                 if newPath[-1:] != os.sep:
                     newPath += os.sep
+            # if currentFilePath != newPath:
             self._ip.run_command('update_input_panel', {'text': newPath})
 
     def on_done_open(self, text):
-        self.suggestions_list.close()
+        self.suggs_list.close()
 
         text = os.path.expanduser(text)
 
@@ -226,11 +294,11 @@ class FilePromptCommand(sublime_plugin.WindowCommand):
             sublime.status_message('Unable to open "%s"' % text)
 
     def on_done_save(self, text):
-        self.suggestions_list.close()
+        self.suggs_list.close()
         self.save_file_to_disk(text)
 
     def on_panel_closed(self):
-        self.suggestions_list.close()
+        self.suggs_list.close()
 
     def save_file_to_disk(self, file_name):
         window = self.window
@@ -274,6 +342,7 @@ class FilePromptCommand(sublime_plugin.WindowCommand):
         file_contents = input_text.encode(encoding)
         return file_contents
 
+
 class ClearFileListCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.set_read_only(False)
@@ -297,14 +366,20 @@ class UpdateInputPanelCommand(sublime_plugin.TextCommand):
 
 
 class OpenFilePrompt(FilePromptCommand):
-    def run(self, use_scratch_buffer=True):
+    def run(self,
+            suggestions_list_type=FilePromptCommand.SUGGS_SCRATCH,
+            directories_first=True):
         self.show_prompt(
             save_file=False,
-            use_scratch_buffer=use_scratch_buffer)
+            suggestions_list_type=suggestions_list_type,
+            dirs_first=directories_first)
 
 
 class SaveFilePrompt(FilePromptCommand):
-    def run(self, use_scratch_buffer=True):
+    def run(self,
+            suggestions_list_type=FilePromptCommand.SUGGS_SCRATCH,
+            directories_first=True):
         self.show_prompt(
             save_file=True,
-            use_scratch_buffer=use_scratch_buffer)
+            suggestions_list_type=suggestions_list_type,
+            dirs_first=directories_first)
